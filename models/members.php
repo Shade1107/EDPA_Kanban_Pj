132b62279b79accc3982be008886eeda80a8a9d9
Shine Lynn Aung <159878453+ShineLynn@users.noreply.github.com>
2024-03-17 22:46:51 +0630

    Update members.php

--- models/members.php
+++ models/members.php
@@ -1,6 +1,6 @@
 <?php
     require_once("models/model.php");
-    require_once("DatabaseConnection.php");
+    require_once("models/DatabaseConnection.php");
 
     class member extends Model{
         private static $table = "members";
