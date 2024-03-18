a13934706515f953e15fbfd8ec95c6387075a3e6
JACK <160564929+SiThuwin2@users.noreply.github.com>
2024-03-18 09:11:01 +0630

    UI

---
+++ kaban/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>JS CSS HTML Kanban Board</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="icon" type="image/png" href="image/logo.PNG">
+  </head>
+  <body>
+    <!-- <header>
+      <div class="info-container">    
+    </div>
+  </header> -->
+<!-- login form -->
+   <div class="loginForm">
+   <form>
+   <h1 class="loginFormText">⟁ Kanban Board</h1>
+  <div class="mb-4 input-group-lg col-auto" >
+    <label for="username" class="form-label">UserName</label> <br>
+    <input type="email"  id="username" class="input-field">
+  
+  </div>
+  <div class="mb-4 input-group-lg col-auto" >
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" id="exampleInputPassword1" class="input-field" >
+  </div>
+  
+  <button type="button" class="button">Login</button>
+ 
+</form>
+
+   </div>
