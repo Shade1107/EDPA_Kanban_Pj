f4d8b17952af424083fcae66a4f3e92936432760
LaYaung8 <lyktp882003@gmail.com>
2024-03-18 14:02:49 +0630

    ly

---
+++ kaban/loginfunc.php
@@ -0,0 +1,43 @@
+<?php
+require_once("DBConnection.php");
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    // $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    // Perform basic validation
+    if ( empty($email) || empty($password)) {
+        echo '
+        <script>
+            window.location.href = "login.php";
+            alert("All fields are required");
+        </script>';
+        exit;
+    }
+
+    // Check if the user already exists in the database
+    $db = new DBConnection();
+    $conn = $db->getConnection();
+    $table = "users";
+    $query = "
+        SELECT * FROM $table WHERE email = '$email' AND password = '$password'
+    ";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        // If the user already exists, redirect to switch.php
+        header("Location: index.php");
+        exit;
+     } else {
+        echo  '
+        <script>
+            window.location.href = "login.php";
+            alert("This user does not exist");
+        </script>';
+    }
+}
+?>
+
---
+++ kaban/signupfunc.php
@@ -0,0 +1,43 @@
+<?php
+require_once("DBConnection.php");
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $db = new DBConnection();
+    $conn = $db->getConnection();
+    $table = "users";
+    $query = "
+        SELECT * FROM $table WHERE name = '$name' AND email = '$email' AND password = '$password'
+    ";
+    $result = $conn->query($query);
+}
+
+if ($result->num_rows > 0) {
+    echo  '
+    <script>
+        window.location.href = "login.php";
+        alert("This user already exist");
+    </script>';
+    exit;
+ } else {
+            // If the user doesn't exist, insert the data into the database
+        $query = "
+            INSERT INTO $table (name, email, password) 
+            VALUES ('$name', '$email', '$password')
+        ";
+        $insert_result = $conn->query($query);
+
+        if ($insert_result) {
+            // Redirect to the index page
+            header("Location: index.php");
+            exit;
+        } else {
+            echo "Failed to insert user data.";
+        }
+ }
+?>
